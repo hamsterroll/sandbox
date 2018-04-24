16343985c986ba956fa1a76005d0e048fdd5921b
Boštjan Ažman <azmanbostjan@gmail.com>
2018-04-24 17:33:15 +0200

    asdfasdfasd

--- start.py
+++ start.py
@@ -1,1 +1,11 @@
 """Prvi program v pythonu"""
+
+def moja_funkcija(a, b):
+	"""
+	:param a: pač en parameter.
+	"""
+	vmesni_rezultat = a + b
+	return vmesni_rezultat - 5
+
+
+print(moja_funkcija(1, 2))
